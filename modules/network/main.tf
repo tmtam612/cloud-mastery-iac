475fd58a61cfeb7bac152a1f06168106c63a8d9f
tmtam612 <tmtam612@gmail.com>
2024-03-30 18:10:48 +0700

    fix bug invalid reference make terraform not work

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -17,14 +17,14 @@ resource "azurerm_virtual_network" "this" {
 }
 
 resource "azurerm_subnet" "subnet1" {
-  name                 = "${var.project_name}-${local.subnet_abbrevation}-${subnet1}-${var.environment}-${var.location}-${var.instance_count}"
+  name                 = "${var.project_name}-${local.subnet_abbrevation}-${local.subnet1}-${var.environment}-${var.location}-${var.instance_count}"
   address_prefixes     = var.subnet1_address_space
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.this.name
 }
 
 resource "azurerm_subnet" "subnet2" {
-  name                 = "${var.project_name}-${local.subnet_abbrevation}-${subnet2}-${var.environment}-${var.location}-${var.instance_count}"
+  name                 = "${var.project_name}-${local.subnet_abbrevation}-${local.subnet2}-${var.environment}-${var.location}-${var.instance_count}"
   address_prefixes     = var.subnet2_address_space
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.this.name
