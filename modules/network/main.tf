8588e2020dd83db41d18eae73494684ca0005099
tmtam612 <tmtam612@gmail.com>
2024-05-19 16:54:10 +0700

    add nsg

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -16,6 +16,33 @@ resource "azurerm_subnet" "subnet1" {
   virtual_network_name = azurerm_virtual_network.this.name
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  # security_rule {
+  #   name                       = "test123"
+  #   priority                   = 100
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Tcp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "*"
+  #   source_address_prefix      = "*"
+  #   destination_address_prefix = "*"
+  # }
+
+  # tags = {
+  #   environment = "Production"
+  # }
+}
+
+resource "azurerm_subnet_network_security_group_association" "assoc" {
+  subnet_id                 = azurerm_subnet.subnet1.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 # resource "azurerm_subnet" "subnet2" {
 #   name                 = "${var.project_name}-${local.subnet_abbrevation}-${local.subnet2}-${var.environment}-${var.location}-${var.instance_count}"
 #   address_prefixes     = var.subnet2_address_space
