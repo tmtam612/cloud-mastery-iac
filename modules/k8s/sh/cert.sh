488c28e49bcc16eac5c04b32527ae08177fd5108
tmtam612 <tmtam612@gmail.com>
2024-05-20 21:28:29 +0700

    update code automatically

--- modules/k8s/sh/cert.sh
+++ modules/k8s/sh/cert.sh
@@ -2,16 +2,22 @@ path_module=$1
 storage_account_name=$2
 container_name=$3
 blob_name=$4
-export dns_zone_name=$5
-export resource_group=$6
-export subscription_id=$7
-export client_id=$8
+export AZURE_ZONE_NAME=$5
+export AZURE_RESOURCE_GROUP=$6
+export AZURE_SUBSCRIPTION_ID=$7
+export IDENTITY_CLIENT_ID=$8
+export ACME_STAGING_SERVER_URL=$9
+export ACME_SERVER_URL=$10
+export EMAIL=$11
+
 envsubst < $PWD/$path_module/yaml/issuer.yaml | kubectl apply --validate=false -f -
 blob_exist=$(az storage blob exists --account-name $storage_account_name --container-name $container_name --name $blob_name)
-# kubectl apply -f $PWD/$path_module/yaml/issuer.yaml --validate=false
 if ! kubectl get namespace ingress-service; then
     kubectl create namespace ingress-service
 fi
+# Set values for dnsNames
+export DNS_NAME=$12
+export DNS_NAME_WILDCARD=$13
 kubectl apply -f $PWD/$path_module/yaml/certificate.yaml --validate=false
 if [[ $(echo "$blob_exist" | jq -r '.exists') == "true" ]]; then
     az storage blob download --account-name $storage_account_name --container-name $container_name  --name $blob_name --file secret.yaml
